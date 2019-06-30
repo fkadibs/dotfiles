468526214b13cac8efdc698eb3aafdda5562dda0
disaster_byte <11808043+disasterbyte@users.noreply.github.com>
2019-06-30 16:15:39 -0500

    basic

---
+++ tmux-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# just in case
+sudo apt-get update && apt-get install tmux
+
+BASE_DIR="$HOME/.tmux"
+PLUGIN_DIR="$BASE_DIR/plugins"
+
+mkdir -p $PLUGIN_DIR
+
+git clone https://github.com/tmux-plugins/tpm $PLUGIN_DIR/tpm
+git clone https://github.com/disasterbyte/dotfiles/master/.tmux.conf -O $HOME/.tmux.conf
+
+echo "tmux setup complete :)"
