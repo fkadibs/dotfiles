267635d8c6bdf720d0f22a6284e3c88f1fbfebc6
disasterbyte <11808043+disasterbyte@users.noreply.github.com>
2019-07-13 21:26:12 -0500

    tweak

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -9,6 +9,6 @@ PLUGIN_DIR="$BASE_DIR/plugins"
 mkdir -p $PLUGIN_DIR
 
 git clone https://github.com/tmux-plugins/tpm $PLUGIN_DIR/tpm
-wget https://raw.github.com/disasterbyte/dotfiles/master/.tmux.conf -O $HOME/.tmux.conf
+cp .tmux.conf -O $HOME/.tmux.conf
 
 echo "tmux setup complete :)"
