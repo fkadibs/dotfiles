002ac274e7af7b93ce0dfa42b72fa0f6d08cebe0
disaster_byte <11808043+disasterbyte@users.noreply.github.com>
2020-01-09 14:01:15 -0600

    Update tmux-setup.sh

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -9,6 +9,6 @@ PLUGIN_DIR="$BASE_DIR/plugins"
 mkdir -p $PLUGIN_DIR
 
 git clone https://github.com/tmux-plugins/tpm $PLUGIN_DIR/tpm
-cp .tmux.conf -O $HOME/.tmux.conf
+cp .tmux.conf $HOME/.tmux.conf
 
 echo "tmux setup complete :)"
