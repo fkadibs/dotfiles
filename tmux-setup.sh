41dd4e87a84d3c0afe04cddfd7f89cdecb5dd068
fka dibs <11808043+fkadibs@users.noreply.github.com>
2021-12-01 20:51:49 -0600

    Update tmux-setup.sh

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -3,12 +3,12 @@
 # just in case
 sudo apt-get update && apt-get install tmux
 
-BASE_DIR="$HOME/.tmux"
+BASE_DIR="~/.tmux"
 PLUGIN_DIR="$BASE_DIR/plugins"
 
 mkdir -p $PLUGIN_DIR
 
 git clone https://github.com/tmux-plugins/tpm $PLUGIN_DIR/tpm
-cp .tmux.conf $HOME/.tmux.conf
+cp .tmux.conf ~/.tmux.conf
 
 echo "tmux setup complete :)"
